310c8769fc9219825b51d044a727676c91450cec
Jimco101 <63980029+Jimco101@users.noreply.github.com>
2020-04-19 18:09:03 -0500

    Add files via upload

---
+++ TASEP.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include <fstream>
+#include<math.h>
+//#include<graphics.h>
+
+using namespace std;
+
+int main()
+{
+	int N=10;
+	int Lattice[N];
+	for (int i = 0; i < N; i++ ) 
+	{		
+		Lattice[i]=0;
+   	} 
+   	for (int i = 0; i < N; i++ ) 
+	{		
+		cout<<Lattice[i]<<" ";
+   	} 
+}
