ec9fc6383194a485d50dbe839583592525b12189
Jimco101 <63980029+Jimco101@users.noreply.github.com>
2020-04-19 18:33:49 -0500

    Add files via upload

--- TASEP.cpp
+++ TASEP.cpp
@@ -6,8 +6,9 @@ using namespace std;
 
 int main()
 {
-	int N=10;
+	int N=10, timer=1000;
 	int Lattice[N];
+	float rng[10000];
 	for (int i = 0; i < N; i++ ) 
 	{		
 		Lattice[i]=0;
@@ -16,7 +17,21 @@ int main()
 	{		
 		cout<<Lattice[i]<<" ";
    	} 
-   	ofstream myfile;
-	myfile.open ("rng.txt");
+   	
+   	string line_;
+   	ifstream myfile;
+	ifstream file_("rngMod.txt");
+	if(file_.is_open())
+	{
+		for(int i = 0; i < 10000; i++)
+		{
+			file_ >> rng[i];
+		}
+		file_.close();
+	}
 	
+	for(int i = 0; i < 10000; i++)
+	{
+		cout<< rng[i]<<endl;
+	}
 }
