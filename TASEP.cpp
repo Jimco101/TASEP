84934ae47c179cf90f7dfd2a1bee7d69c1b5a14e
Jimco101 <63980029+Jimco101@users.noreply.github.com>
2020-04-19 19:50:28 -0500

    Add files via upload

--- TASEP.cpp
+++ TASEP.cpp
@@ -6,17 +6,18 @@ using namespace std;
 
 int main()
 {
-	int N=10, timer=1000;
+	int N=10, timer=1000, j=0, k=0;
+	float q=0.5, p=1.0;
 	int Lattice[N];
 	float rng[10000];
 	for (int i = 0; i < N; i++ ) 
 	{		
 		Lattice[i]=0;
    	} 
-   	for (int i = 0; i < N; i++ ) 
-	{		
-		cout<<Lattice[i]<<" ";
-   	} 
+//   	for (int i = 0; i < N; i++ ) 
+//	{		
+//		cout<<Lattice[i]<<" ";
+//   	} 
    	
    	string line_;
    	ifstream myfile;
@@ -29,9 +30,35 @@ int main()
 		}
 		file_.close();
 	}
-	
-	for(int i = 0; i < 10000; i++)
+	while(k<timer)
 	{
-		cout<< rng[i]<<endl;
+		if(p > rng[j] && Lattice[1] == 0)
+		{
+			Lattice[0]=1;
+		}
+		
+		for(int i = N; i >= 0; i--)
+		{
+			if(q > rng[j] && Lattice[i]==1 && Lattice[i+1] == 0)
+			{
+				Lattice[i+1]=1;
+				Lattice[i]=0;
+			}
+
+			j++;
+		}
+		
+		if(q > rng[j] && Lattice[N] == 1)
+		{
+			Lattice[N]=0;
+		}
+		
+		for (int h = 0; h < N; h++ ) 
+		{		
+			cout<<Lattice[h]<<" ";
+	 	} 
+	 	cout<<endl;
+		k++;
 	}
 }
+
